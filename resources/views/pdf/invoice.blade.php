e07f077a0fcecbbbe08be97e197c99f1c972d5cd
NandoFBP <nando.bp87@gmail.com>
2016-05-24 23:08:40 +0200

    Haciendo vista del pdf, hay que hacerla a mano y con tablas y usar lo mínimo HTML5. PD: Fuck tables, PD2:Fuck Datepicker too.

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -3,42 +3,211 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Example 2</title>
-    {!! Html::style('assets/css/pdf.css') !!}
+
+    <style type="text/css">
+@page {
+            margin-top: 0.2em;
+            margin-left: 0.0em;
+
+        }
+        table{
+          border:1px black solid;
+        }
+        tr{
+          border:1px black solid;
+           height: 10px;
+        }
+        td{
+          border:1px black solid;
+          height: 10px;
+        }
+.clearfix:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+a {
+  color: #5D6975;
+  text-decoration: underline;
+}
+
+body {
+  position: relative;
+  width: 21cm;  
+  height: 29.7cm; 
+  margin: 0 auto; 
+  color: #001028;
+  background: #FFFFFF; 
+  font-family: Arial, sans-serif; 
+  font-size: 12px; 
+  font-family: Arial;
+}
+
+header {
+  padding: 10px 0;
+  margin-bottom: 30px;
+}
+
+#logo {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+#logo img {
+  width: 90px;
+}
+
+h1 {
+  border-top: 1px solid  #5D6975;
+  border-bottom: 1px solid  #5D6975;
+  color: #5D6975;
+  font-size: 2.4em;
+  line-height: 1.4em;
+  font-weight: normal;
+  text-align: center;
+  margin: 0 0 20px 0;
+  background: url(dimension.png);
+}
+
+#project {
+  float: left;
+}
+
+#project span {
+  color: #5D6975;
+  text-align: right;
+  width: 52px;
+  margin-right: 10px;
+  display: inline-block;
+  font-size: 0.8em;
+}
+
+#company {
+  float: right;
+  text-align: right;
+}
+
+#project div,
+#company div {
+  white-space: nowrap;        
+}
+
+table {
+
+  border-collapse: collapse;
+
+}
+
+
+
+
+
+
+
+#notices .notice {
+  color: #5D6975;
+  font-size: 1.2em;
+}
+
+footer {
+  color: #5D6975;
+  width: 100%;
+  height: 30px;
+  position: absolute;
+  bottom: 0;
+  border-top: 1px solid #C1CED9;
+  padding: 8px 0;
+  text-align: center;
+}
+
+</style>
   </head>
   <body>
 
-    <main>
+
       <div id="details" class="clearfix">
         <div id="invoice">
-          <h1>INVOICE {{ $invoice }}</h1>
-          <div class="date">Date of Invoice: {{ $date }}</div>
+          <h1 style="text-align:center">Currículum Vitae</h1>
+
+          <!-- Tabla información personal -->
+            <table style="margin-left:5px; margin-top:50px">
+            <tr>
+            <td colspan="2" style="width:300px;"><span style="color:darkblue; margin-left:100px; text-transform:uppercase">Información Personal</span></td>
+            <td style="width:475px;"><span style="text-transform:capitalize; font-size:80%"></span></td> 
+            </tr>
+             <tr>
+            <td rowspan="5" style="width:300px;"><img style="margin-left:145px" src="img/icon.png" alt="" width="70" height="70"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">nombre y apellidos</span></td>
+            </tr>
+             <tr>
+            <td style="width:475px;"><span style="text-transform:capitalize">dirección, código postal,población,provincia</span></td>
+            </tr>
+             <tr>
+            <td style="width:475px;"><span style="text-transform:capitalize">teléfono</span></td>
+            </tr>
+             <tr>
+            <td style="width:475px;"><span>email</span></td>
+            </tr>
+             <tr>
+            <td style="width:475px;"><span style="margin-right;10px" >Redes sociales, Nombre red social :  nombre cuenta.</span></td>
+            </tr>
+             <tr>
+            <td rowspan="5" style="width:300px;"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Fecha de nacimiento y nacionalidad</span></td>   
+            </tr>
+          </table>
+
+          <!-- Tabla experiencia profesional -->
+          <table style="margin-left:5px; margin-top:20px">
+            <tr>
+            <td style="width:300px;"><span style="color:darkblue; margin-left:100px; text-transform:uppercase">Experiencia Profesional</span></td>
+            <td style="width:475px;"><span style="text-transform:capitalize"></span></td>            
+            </tr>
+             <tr>
+            <td style="width:300px;"><span style="margin-left:113px">(01-10-2006 –01- 04-2010)</span></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Nombre del empleo</span></td>         
+            </tr>
+             <tr>
+            <td style="width:300px;"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Empresa en la que trabajó/a. Ciudad y Provincia</span></td>         
+            </tr>
+             <tr>
+            <td style="width:300px;"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Descripción</span></td>
+            </tr>  
+          </table>
+
+          <!-- Tabla educación y formación -->
+          <table style="margin-left:5px; margin-top:20px">
+            <tr>
+            <td style="width:300px;"><span style="color:darkblue; margin-left:100px; text-transform:uppercase">Educación y Formación</span></td>
+            <td style="width:475px;"><span style="text-transform:capitalize"></span></td>
+            
+            </tr>
+             <tr>
+            <td style="width:300px;"><span style="margin-left:113px">(2006–2010) ó (2015-cursando)</span></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Nombre del título</span></td>
+            
+            </tr>
+             <tr>
+            <td style="width:300px;"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Centro en el que se obtuvo el título. Localidad y Provincia</span></td>
+            
+            </tr>
+             <tr>
+            <td style="width:300px;"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Ciudad y provincia</span></td>
+            
+            </tr>
+             <tr>
+            <td style="width:300px;"></td>
+            <td style="width:475px;"><span style="text-transform:capitalize">Descripción</span></td>
+            
+            </tr>
+            
         </div>
       </div>
-      <table border="0" cellspacing="0" cellpadding="0">
-        <thead>
-          <tr>
-            <th class="no">#</th>
-            <th class="desc">DESCRIPTION</th>
-            <th class="unit">UNIT PRICE</th>
-            <th class="total">TOTAL</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="no">{{ $data['quantity'] }}</td>
-            <td class="desc">{{ $data['description'] }}</td>
-            <td class="unit">{{ $data['price'] }}</td>
-            <td class="total">{{ $data['total'] }} </td>
-          </tr>
-
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="2"></td>
-            <td >TOTAL</td>
-            <td>$6,500.00</td>
-          </tr>
-        </tfoot>
-      </table>
+      
   </body>
 </html>
