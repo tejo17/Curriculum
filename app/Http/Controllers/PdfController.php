4ccd3e1dd4f94ece6f6369cd255c9106b0bfc4fa
NandoFBP <nando.bp87@gmail.com>
2016-06-02 12:17:35 +0200

    pdf controller

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -5,31 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+
 use Session;
+
+use Auth;
 
 class PdfController extends Controller
 {
-    public function invoice() 
+    public function invoice(Request $request) 
     {
-        $data = $this->getData();
-        $date = date('Y-m-d');
-        $invoice = "2222";
-        $view =  \View::make('pdf.curriculum', compact('data', 'date', 'invoice'))->render();
+
+        $datos = $request->all();
+        $view =  \View::make('pdf.curriculum', compact('datos'))->render();
         $pdf = \App::make('dompdf.wrapper');
         $pdf->loadHTML($view);
         return $pdf->stream('invoice');
     }
 
-    public function getData() 
-    {
-
-        $data =  [
-            'quantity'      => '100',
-            'description'   => 'some ramdom text',
-            'price'   => '500',
-            'total'     => '500'
-        ];
-        dd($data);
-        return $data;
-    }
+    
 }
