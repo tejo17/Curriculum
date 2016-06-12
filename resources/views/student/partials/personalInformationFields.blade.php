46ef5708f6e54aeb52b8e5e14f8de699fceee194
NandoFBP <nando.bp87@gmail.com>
2016-06-12 16:17:51 +0200

    unión o borrado de algunos ficheros de los formularios

--- resources/views/student/partials/personalInformationFields.blade.php
+++ resources/views/student/partials/personalInformationFields.blade.php
@@ -99,7 +99,20 @@
             </span>
         @endif
     </div>
-    @include('student.partials.emailField')
+    <div class=" col-md-6 control-group{{ $errors->has('email') ? ' has-error' : '' }}">
+        <div class="row">
+            <div class="input-field col-md-12">
+                <i class="material-icons prefix">account_circle</i>
+                {{ Form::text('email', null,['id' => "email"]) }}
+                {{ Form::label('email', 'Email') }}
+            </div>
+        </div>
+        @if ($errors->has('email'))
+        <span class="help-block">
+            <strong>{{ $errors->first('email') }}</strong>
+        </span>
+        @endif
+    </div>
     
      <fieldset style="clear:both">    
     <legend style="width:auto;">Direccion</legend>
